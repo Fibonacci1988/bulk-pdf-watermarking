751aa0b202ab0c94c44d304adf38cd954d8f03d9
Shyngys <zhakelov.shyngys@gmail.com>
2020-09-04 13:45:51 +0500

    first version on heroku

---
+++ api/api.py
@@ -0,0 +1,92 @@
+from flask import Flask
+from flask import send_file
+from PyPDF2 import PdfFileWriter, PdfFileReader
+from reportlab.pdfgen.canvas import Canvas
+import os
+import glob
+import requests
+import zipfile
+
+from flask import render_template, request
+from werkzeug import secure_filename
+
+
+app =Flask(__name__)
+
+
+	
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_files():
+   if request.method == 'POST':
+      f = request.files['file']
+      f.save('files/'+secure_filename(f.filename))
+      return 'file uploaded successfully'
+		
+if __name__ == '__main__':
+   app.run(debug = True)
+
+
+
+
+
+@app.route('/api',methods=['GET','POST'])
+
+
+def api():
+    watermark = 'template/template.pdf'
+    watermark_obj = PdfFileReader(watermark)
+    watermark_page = watermark_obj.getPage(0)
+    for file in os.listdir('files'):
+        if '.pdf' in file:
+            print(file)
+            pdf_reader = PdfFileReader('files/'+file)
+            pdf_writer = PdfFileWriter()
+        
+            # Watermark all the pages
+            for page in range(pdf_reader.getNumPages()):
+                
+                page = pdf_reader.getPage(page)
+                page.mergePage(watermark_page)
+                pdf_writer.addPage(page)
+                
+                with open('files/'+file, 'wb') as out:
+                    pdf_writer.write(out)
+
+    ziph=zipfile.ZipFile('result/Result.zip','w',zipfile.ZIP_DEFLATED)
+    path='files/'
+    for root,dirs,files in os.walk(path):
+        for file in files:
+            ziph.write(os.path.join(root,file))
+    ziph.close()
+
+    deletionFiles=glob.glob('files/*')
+    for f in deletionFiles:
+        os.remove(f)
+    
+
+    return send_file('result/Result.zip',as_attachment=True)
+
+    
+@app.route('/changeWatermarkText', methods = ['GET', 'POST'])
+def change_watermark_text():
+   
+   deletionFiles=glob.glob('template/*')
+   for f in deletionFiles:
+       os.remove(f)
+   
+   canvas = Canvas("template/template.pdf")
+   canvas.setFont("Times-Roman", 36)
+   canvas.drawString(150, 300, "Shyngys Text")
+   canvas.save()
+
+   #pdf_reader = PdfFileReader('template/template.pdf')
+   #pdf_writer = PdfFileWriter()
+
+   #page = pdf_reader.getPage(0)
+   #page.rotateClockwise(315)
+   #pdf_writer.addPage(page)
+   #pdf_writer.write('template/template2.pdf')
+
+
+   return 'Watermark changed successfully'
+
--- src/App.js
+++ src/App.js
@@ -1,26 +1,191 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import 'antd/dist/antd.css';
 import './App.css';
+import styled from "styled-components";
+import background from './background.jpg';
+import Main from './Main.jsx'
+import { Document, Page, pdfjs } from 'react-pdf';
+import samplePDF from './test.pdf';
+import { Upload, message } from 'antd';
+import { InboxOutlined } from '@ant-design/icons';
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
+const { Dragger } = Upload;
+
+const props = {
+  name: 'file',
+  multiple: true,
+  action: 'http://localhost:3000/uploader',
+  onChange(info) {
+    const { status } = info.file;
+    if (status !== 'uploading') {
+      console.log(info.file, info.fileList);
+    }
+    if (status === 'done') {
+      message.success(`${info.file.name} file uploaded successfully.`);
+    } else if (status === 'error') {
+      message.error(`${info.file.name} file upload failed.`);
+    }
+  },
+};
+
+const MainContainer = styled.div`
+width: 1440px;
+height: 1024px;
+`;
+
+const MainHeader=styled.header`
+width: 1440px;
+height: 80px;
+background: #FAFAFA;
+`
+const MainTitle=styled.header`
+width: 666px;
+height: 56px;
+font-family: Inter;
+font-style: normal;
+font-weight: 800;
+font-size: 48px;
+line-height: 120%;
+text-align: center;
+color: black;
+margin:auto;
+`
+
+
+
+const PageColumn=styled.div`
+width: 475px;
+height: 945px;
+float:left
+background:#EDCFA9;
+border: 5px solid #000000;
+padding: 15px
+`
+
+
+
+
+const UploadWindow=styled.div`
+position: absolute;
+width: 886px;
+height: 288px;
+left: 277px;
+top: 300px;
+background: #FAFAFA;
+border: 1px dashed #000000;
+box-sizing: border-box;
+`
+
+const UploadWindowMessage=styled.header`
+width: 487px;
+height: 91px;
+left: 477px;
+top: 399px;
+`
+
+const ButtonUpload=styled.button`
+display: flex;
+flex-direction: row;
+padding: 8px 12px;
+
+width: 197px;
+height: 40px;
+left: 622px;
+top: 450px;
+
+background: #457B9D;
+/* Blue / 400 */
+
+border: 1px solid #0052CC;
+box-sizing: border-box;
+border-radius: 3px;
+`
+
+const AddRemoveButton=styled.button`
+display: flex;
+flex-direction: row;
+padding: 24px 48px;
+width: 334px;
+height: 86px;
+margin: auto;
+
+background: #1D3557;
+/* Blue / 400 */
+
+border: 1px solid #0052CC;
+box-sizing: border-box;
+border-radius: 3px;
+`
+
+const emptyDiv = styled.div``;
+
 
 function App() {
+  
+  
+  useEffect(()=>{
+    fetch('/api').then(
+      response=>response.json()
+    )
+
+  },[]);
+
+  
+
+
+
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MainContainer>
+      
+    <MainHeader>
+    <MainTitle>Watermark PDF files online</MainTitle>
+    </MainHeader>
+
+
+    <div class="holy-grail-body">
+
+			<section class="holy-grail-content">
+      
+      <Dragger {...props}>
+    <p className="ant-upload-drag-icon">
+      <InboxOutlined />
+    </p>
+    <p className="ant-upload-text">Click or drag file to this area to upload</p>
+  </Dragger>
+
+			</section>
+
+			<div class="holy-grail-sidebar-1 hg-sidebar">
+      
+      <form action="/changeWatermarkText" method="post">
+      <AddRemoveButton>Change Watermark Text</AddRemoveButton>
+    </form>
+
+
+			</div>
+
+			<div class="holy-grail-sidebar-2 hg-sidebar">
+      <form action="/api" method="post">
+      <AddRemoveButton>Add Watermark</AddRemoveButton>
+    </form>
+
+			</div>
+
+		</div>
+
+
+
+   
+    
+    
+    </MainContainer>
   );
 }
 
 export default App;
+
